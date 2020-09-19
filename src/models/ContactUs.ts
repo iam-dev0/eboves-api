3d107ac890e2e66aa27c11f6359a0eaf5d89e98c
awais000 <awaismanzoor00@gmail.com>
2020-09-19 20:13:36 +0500

    contact us modal error

--- src/models/ContactUs.ts
+++ src/models/ContactUs.ts
@@ -1,60 +1,52 @@
 import {
-    Column,
-    DataType,
-    Model,
-    Table,
-    Default,
-    Scopes,
-    AllowNull,
-    PrimaryKey,
-    AutoIncrement,
-    IsUrl,
-    IsEmail,
-    NotNull,
-  } from "sequelize-typescript";
-  
-  @Table({
-    paranoid: false,
-    timestamps: false,
-    tableName: "contact_us",
-  })
-  @Scopes({
-    website: {
-      where: { active: true },
-    },
-  })
+  Column,
+  DataType,
+  Model,
+  Table,
+  Default,
+  Scopes,
+  AllowNull,
+  PrimaryKey,
+  AutoIncrement,
+  IsUrl,
+  IsEmail,
+  NotNull,
+} from "sequelize-typescript";
 
-  export class ContactUs extends Model<ContactUs> {
-    @AllowNull(false)
-    @PrimaryKey
-    @AutoIncrement
-    @Column({
-      type: DataType.INTEGER.UNSIGNED,
-    })
-    id!: number;
-  
-    @NotNull
-    @Column
-    name!: string;
-  
-    @NotNull
-    @IsEmail
-    @Column
-    email!: string;
-  
-    @NotNull
-    @Column
-    phone!: string;
-  
-    @NotNull
-    @Column
-    subject!: string;
-  
-    @NotNull
-    @Column
-    message!: string;
-  
-  }
-  
-  export default ContactUs;
-  
+@Table({
+  paranoid: false,
+  timestamps: false,
+  tableName: "contact_us",
+})
+@Scopes({
+  website: {
+    where: { active: true },
+  },
+})
+export class ContactUs extends Model<ContactUs> {
+  @AllowNull(false)
+  @PrimaryKey
+  @AutoIncrement
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+  })
+  id!: number;
+
+  @Column
+  name!: string;
+
+  @IsEmail
+  @Column
+  email!: string;
+
+  @Column
+  phone!: string;
+
+  @Column
+  subject!: string;
+
+  @Column
+  message!: string;
+}
+
+export default ContactUs;
